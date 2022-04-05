80f152e156c06900155dc47e8c1c2cd67e19138e
agung senjaya <agungsenjaya813@gmail.com>
2022-04-05 23:46:42 +0700

    1

---
+++ packages/uikit/src/components/Footer/config.tsx
@@ -0,0 +1,68 @@
+import { Language } from "../LangSelector/types";
+import { FooterLinkType } from "./types";
+
+export const footerLinks: FooterLinkType[] = [
+  {
+    label: "About",
+    items: [
+      {
+        label: "Website",
+        href: "https://docs.pancakeswap.finance/contact-us",
+      },
+      {
+        label: "Community",
+        href: "https://docs.pancakeswap.finance/contact-us/telegram",
+      },
+    ],
+  },
+  {
+    label: "Help",
+    items: [
+      {
+        label: "Customer",
+        href: "Support https://docs.pancakeswap.finance/contact-us/customer-support",
+      },
+      {
+        label: "Troubleshooting",
+        href: "https://docs.pancakeswap.finance/help/troubleshooting",
+      },
+    ],
+  },
+  {
+    label: "Developers",
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/pancakeswap",
+      },
+      {
+        label: "Careers",
+        href: "https://docs.pancakeswap.finance/hiring/become-a-chef",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Twitter",
+    icon: "Twitter",
+    href: "https://twitter.com/pancakeswap",
+  },
+  {
+    label: "Telegram",
+    icon: "Telegram",
+    href: "https://twitter.com/pancakeswap",
+  },
+  {
+    label: "Discord",
+    icon: "Discord",
+    href: "https://discord.gg/pancakeswap",
+  },
+];
+
+export const langs: Language[] = [...Array(20)].map((_, i) => ({
+  code: `en${i}`,
+  language: `English${i}`,
+  locale: `Locale${i}`,
+}));
---
+++ src/components/Menu/config/config.ts
@@ -0,0 +1,35 @@
+import {
+  MenuItemsType,
+  DropdownMenuItemType,
+  SwapIcon,
+  SwapFillIcon,
+  EarnFillIcon,
+  EarnIcon,
+  TrophyIcon,
+  TrophyFillIcon,
+  NftIcon,
+  NftFillIcon,
+  MoreIcon,
+} from '@pancakeswap/uikit'
+import { ContextApi } from 'contexts/Localization/types'
+import { nftsBaseUrl } from 'views/Nft/market/constants'
+import { perpLangMap } from 'utils/getPerpetualLanguageCode'
+
+export type ConfigMenuItemsType = MenuItemsType & { hideSubNav?: boolean }
+
+const config: (t: ContextApi['t'], languageCode?: string) => ConfigMenuItemsType[] = (t, languageCode) => [
+  {
+    label: t('Trade'),
+    fillIcon: SwapFillIcon,
+    href: '/swap',
+    showItemsOnMobile: false,
+    items: [
+      {
+        label: t('Swap'),
+        href: '/swap',
+      },
+    ],
+  },
+]
+
+export default config
